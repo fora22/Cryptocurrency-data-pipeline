4a08b33b6056d5daa601bf6adefae38821d44fe2
FoRA <48875566+fora22@users.noreply.github.com>
2022-04-09 18:41:18 +0900

    kafka to hbase 실행 py 파일

--- Kafka/kafka_consumer.py
+++ Kafka/kafka_consumer.py
@@ -1,13 +1,15 @@
-from cassandra.cluster import Cluster
-from cassandra.query import tuple_factory
 from kafka import KafkaConsumer 
 from json import loads 
+from conn_hbase import connect_to_hbase
+from conn_hbase import insert_row
+import time
 
+start_time = time.time()
 # topic, broker list 
 consumer = KafkaConsumer( 
     'test', 
-    bootstrap_servers=['localhost:9092'], 
-    auto_offset_reset='earliest',       # latest
+    bootstrap_servers=['localhost:9093'], 
+    auto_offset_reset='none',       # latest, earliest, none
     enable_auto_commit=True, 
     group_id='my-group', 
     value_deserializer=lambda x: loads(x.decode('utf-8')), 
@@ -27,40 +29,52 @@ for message in consumer:
     messageList += message.value
 
 
-cluster = Cluster(['localhost'])
-session = cluster.connect('mykeyspace')
-session.row_factory = tuple_factory
+hostname = 'localhost'
+port=16010
+namespace = 'ecommerce'
+table_name = 'logs'
+batch_size = 1000
+row_count = 0
+start_time = time.time()
+
+conn, table, batch = connect_to_hbase(
+    hostname=hostname,
+    port=port,
+    namespace=namespace,
+    table_name=table_name,
+    batch_size=1000
+    )
+print("Connect to HBase. table name: %s, batch size: %i" % (table_name, batch_size))
+
+import struct
+def float_to_bin(value):  # For testing.
+    """ Convert float to 64-bit binary string. """
+    [d] = struct.unpack(">Q", struct.pack(">d", value))
+    return '{:064b}'.format(d)
 
 for msg in messageList:
-    sqlQuery = f'''
-    INSERT INTO mykeyspace.eCommerce_log 
-    (
-        user_id, 
-        user_session, 
-        event_date, 
-        event_time, 
-        event_type, 
-        product_id, 
-        category_id, 
-        category_code, 
-        brand, 
-        price
-    )
-    VALUES
-    (
-        {msg['user_id']},
-        '{msg['user_session']}',
-        '{msg['event_date']}',
-        '{msg['event_time']}',
-        '{msg['event_type']}',
-        {msg['product_id']},
-        {msg['category_id']},
-        '{msg['category_code']}',
-        '{msg['brand']}',
-        {msg['price']}
-    )
-    ;
-    '''
-    session.execute(sqlQuery)
+    row_count += 1
+    if msg['category_code'] == None:
+        msg['category_code'] = ''
+    if msg['brand'] == None:
+        msg['brand'] = ''
 
-print(messageList)
+    row = [
+        (msg['event_date'] + '_' + str(msg['user_id'])).encode('ascii'),   # Rowkey -> '2019-11-20_558420307'
+        str(msg['user_id']).encode('ascii'),         # u:u_id -> 558420307
+        (msg['user_session']).encode('ascii'),    # u:u_se -> '2761af5c-303c-4a1d-b9fc-46c4e05b1f6c'
+        (msg['event_date']).encode('ascii'),      # t:ed -> '2019-11-20'
+        (msg['event_time']).encode('ascii'),      # t:et -> '2019-11-20 06:33:23 UTC'
+        (msg['event_type']).encode('ascii'),      # pd:et -> 'view
+        str(msg['product_id']).encode('ascii'),      # pd:pi -> 13300069
+        str(msg['category_id']).encode('ascii'),     # pd:ci -> 2053013557166998015
+        (msg['category_code']).encode('ascii'),   # pd:cc -> None or 'appliances.environment.vacuum'
+        (msg['brand']).encode('ascii'),           # pd:br -> None or 'xiaomi'
+        str(msg['price']).encode('ascii')           # pd.pr -> 164.71
+    ]
+    insert_row(batch=batch, row=row)        # batch_size만큼 가득차면 send()함
+
+batch.send()        # 남은 데이터 send()
+conn.close()
+duration = time.time() - start_time
+print("Done. row count: %i, duration: %.3f s" % (row_count, duration))
